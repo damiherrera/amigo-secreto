2db8ddb6fc7bd029b8f04c1a379de7607cb29fa5
Damian Herrera <herreradamian05@gmail.com>
2025-03-09 21:49:01 -0300

    Actualiza README e implementa funcionalidades

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# amigo-secreto
+# Amigo Secreto
+
+## Descripción
+
+Amigo Secreto es una aplicación web que permite a los usuarios ingresar nombres y realizar un sorteo para determinar quién es el amigo secreto de cada participante. La aplicación mostrará el resultado del sorteo en la pantalla de manera sencilla y divertida.
+
+## Características
+
+- Ingreso de Nombres: Permite a los usuarios agregar sus nombres para participar en el sorteo.
+- Sorteo Aleatorio: Realiza un sorteo completamente aleatorio para seleccionar un amigo secreto para cada participante.
+- Interfaz Amigable: Presenta una interfaz fácil de usar para facilitar la interacción con los usuarios.
+- Visualización de Resultados: Muestra los resultados del sorteo de manera clara y animada.
+
+## Tecnologías Utilizadas
+
+- HTML: Para la estructura de la página web.
+- CSS: Para el diseño y estilo de la interfaz.
+- JavaScript: Para la lógica del sorteo y la interactividad.
--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigosIngresados = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+
+    if(nombre === '') {
+        alert('Ingrese un nombre valido');
+        return;
+    } 
+
+    amigosIngresados.push(nombre);
+
+    let lista = document.getElementById('listaAmigos');
+    let nuevoAmigo = document.createElement('li');
+    nuevoAmigo.textContent = nombre;
+    lista.appendChild(nuevoAmigo);
+
+    // Limpiar el campo de entrada
+    document.getElementById('amigo').value = '';
+}
+function sortearAmigo() {
+    if (amigosIngresados.length === 0) {
+        alert('No hay amigos ingresados para sortear');
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigosIngresados.length);
+    let amigoSorteado = amigosIngresados[indiceAleatorio];
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
+    let amigoElemento = document.createElement('li');
+    amigoElemento.textContent = `El amigo secreto sorteado es: ${amigoSorteado}`;
+    resultado.appendChild(amigoElemento);
+
+    document.getElementById('listaAmigos').innerHTML = '';
+    amigosIngresados = [];
+}
+
+
+
+
